1d860cbcf936df4dffd3ecce70d198a1e0d291b8
candy12t <candy12t@gmail.com>
2020-01-31 02:32:49 +0900

    rename

--- src/tk.py
+++ src/tk.py
@@ -5,6 +5,7 @@ from tkinter import filedialog
 from tkinter import ttk
 from tkinter import messagebox
 from video2img import Video
+from audio import Audio
 
 
 class Application:
@@ -34,17 +35,31 @@ class Application:
         filepath = filedialog.askopenfilename(filetypes = fTyp, initialdir = iDir)
         self.file.set(filepath)
 
+    def load_wave(self):
+        fTyp = [('', '*.wav')]
+        iDir = os.path.abspath(os.path.dirname(os.path.dirname(__file__)))
+        filepath = filedialog.askopenfilename(filetypes = fTyp, initialdir = iDir)
+        self.file.set(filepath)
+
     def load_folder(self):
         iDir = os.path.abspath(os.path.dirname(os.path.dirname(__file__)))
         dirc = filedialog.askdirectory(initialdir = iDir)
         self.file.set(dirc)
 
 
-def run():
+def run_video():
     video = Video(file0.get(), file1.get(), file2.get(), file3.get(), file4.get())
     video.read_video()
     video.write()
     video.open_dir()
+    exit()
+
+
+def run_audio():
+    audio = Audio(file0.get(), file2.get())
+    audio.read()
+    audio.write(int(file1.get()))
+    audio.open_dir()
     exit()
 
 
@@ -68,33 +83,60 @@ if __name__ == '__main__':
     frame = ttk.Frame(root, padding=10)
     frame.grid()
 
-    flag0 = False
-    text0 = '処理する動画ファイル'
+    text0 = '処理する音声ファイル'
     num0 = 0
-    file0 = create(frame, text0, num0, flag0)
+    app0 = Application(frame, text0, num0)
+    cmd0 = app0.load_wave
+    app0.label()
+    app0.textbox()
+    app0.btn(cmd0)
+    file0 = app0.file
 
-    flag1 = True
-    text1 = 'RGBカラー画像の出力先フォルダ'
+    text1 = '切り出すフレーム数'
     num1 = 1
-    file1 = create(frame, text1, num1, flag1)
+    app1 = Application(frame, text1, num1)
+    # cmd1 = app1.load_folder
+    app1.label()
+    app1.textbox()
+    # app1.btn(cmd1)
+    file1 = app1.file
 
-    text2 = 'R成分画像の出力先フォルダ'
+    text2 = '出力先フォルダ'
     num2 = 2
-    file2 = create(frame, text2, num2, flag1)
+    app2 = Application(frame, text2, num2)
+    cmd2 = app2.load_folder
+    app2.label()
+    app2.textbox()
+    app2.btn(cmd2)
+    file2 = app2.file
+    
+    # flag0 = False
+    # text0 = '処理する動画ファイル'
+    # num0 = 0
+    # file0 = create(frame, text0, num0, flag0)
 
-    text3 = 'G成分画像の出力先フォルダ'
-    num3 = 3
-    file3 = create(frame, text3, num3, flag1)
+    # flag1 = True
+    # text1 = 'RGBカラー画像の出力先フォルダ'
+    # num1 = 1
+    # file1 = create(frame, text1, num1, flag1)
 
-    text4 = 'B成分画像の出力先フォルダ'
-    num4 = 4
-    file4 = create(frame, text4, num4, flag1)
+    # text2 = 'R成分画像の出力先フォルダ'
+    # num2 = 2
+    # file2 = create(frame, text2, num2, flag1)
+
+    # text3 = 'G成分画像の出力先フォルダ'
+    # num3 = 3
+    # file3 = create(frame, text3, num3, flag1)
+
+    # text4 = 'B成分画像の出力先フォルダ'
+    # num4 = 4
+    # file4 = create(frame, text4, num4, flag1)
 
 
     subFrame = ttk.Frame(frame, padding=10)
     subFrame.grid(row=5, column=1)
 
-    start_button = ttk.Button(subFrame, text='Run', command=run)
+    start_button = ttk.Button(subFrame, text='Run', command=run1)
     start_button.pack(side=LEFT)
 
     exit_button = ttk.Button(subFrame, text='Cancel', command=quit)
