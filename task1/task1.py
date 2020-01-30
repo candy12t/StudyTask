2cf96e9b6a46534f9b21921b5fc311ac3a20671e
candy12t <candy12t@gmail.com>
2020-01-31 01:18:34 +0900

    クラス化

--- task1/task1.py
+++ task1/task1.py
@@ -5,40 +5,72 @@ import cv2
 import os
 
 
-def process(video_path, rgb_img_path, r_img_path, g_img_path, b_img_path):
-    def img_save(img, subdir):
-        img = Image.fromarray(img)
-        img.save('{}/{}.{}'.format(subdir, n, 'png'))
+class Video:
+    def __init__(self, mp4, rgb, r, g, b):
+        self.mp4 = mp4
+        self.rgb = rgb
+        self.r = r
+        self.g = g
+        self.b = b
 
-    video = cv2.VideoCapture(video_path)
-
-    if not video.isOpened():
-        return
-
-
-    n = 0
-    # while True:
-    for i in range(1):
-        ret, frame = video.read()
-        if ret:
-            RGB_img = np.array(frame)
-
-            R_img = RGB_img.copy()
-            R_img[:, :, (1, 2)] = 0
-            G_img = RGB_img.copy()
-            G_img[:, :, (0, 2)] = 0
-            B_img = RGB_img.copy()
-            B_img[:, :, (0, 1)] = 0
-
-            img_save(R_img, r_img_path)
-            img_save(G_img, g_img_path)
-            img_save(B_img, b_img_path)
-            RGB_img = cv2.cvtColor(RGB_img, cv2.COLOR_BGR2RGB)
-            img_save(RGB_img, rgb_img_path)
-
-            n += 1
-        else:
+    def read_video(self):
+        self.video = cv2.VideoCapture(self.mp4)
+        if not self.video.isOpened():
             return
 
-    cmd = 'open {} {} {} {}'.format(rgb_img_path, r_img_path, g_img_path, b_img_path)
-    subprocess.call(cmd.split())
+    # def save_img(self, img, dirc):
+    #     img = Image.fromarray(img)
+    #     img.save('{}/{}.{}'.format(dirc, n, 'png'))
+
+    def write(self):
+        n = 0
+        for i in range(1):
+            ret, frame = self.video.read()
+            if ret:
+                self.rgb_img = np.array(frame)
+                r_img = self.rgb_img.copy()
+                r_img[:, :, (1, 2)] = 0
+                r_img = Image.fromarray(r_img)
+                r_img.save('{}/{}.{}'.format(self.r, n, 'png'))
+
+                g_img = self.rgb_img.copy()
+                g_img[:, :, (0, 2)] = 0
+                g_img = Image.fromarray(g_img)
+                g_img.save('{}/{}.{}'.format(self.g, n, 'png'))
+
+                b_img = self.rgb_img.copy()
+                b_img[:, :, (0, 1)] = 0
+                b_img = Image.fromarray(b_img)
+                b_img.save('{}/{}.{}'.format(self.b, n, 'png'))
+
+
+                # save_img(r_img, self.r)
+                # save_img(g_img, self.g)
+                # save_img(b_img, self.b)
+                self.rgb_img = cv2.cvtColor(self.rgb_img, cv2.COLOR_BGR2RGB)
+                self.rgb_img = Image.fromarray(self.rgb_img)
+                self.rgb_img.save('{}/{}.{}'.format(self.rgb, n, 'png'))
+
+                # save_img(self.rgb_img, self.rgb)
+
+                n += 1
+            else:
+                return
+
+    def open_dir(self):
+        input_dir = os.path.dirname(os.path.abspath(self.mp4))
+        cmd = 'open {} {} {} {} {}'.format(input_dir, self.rgb, self.r, self.g, self.b)
+        subprocess.call(cmd.split())
+
+
+if __name__ == '__main__':
+    mp4 = input()
+    rgb = input()
+    r = input()
+    g = input()
+    b = input()
+
+    video = Video(mp4, rgb, r, g, b)
+    video.read_video()
+    video.write()
+    video.open_dir()
