2fb0e5203807551abaa8c72c83db614b98786726
candy12t <candy12t@gmail.com>
2020-02-02 22:38:03 +0900

    修正

--- src/task1.py
+++ src/task1.py
@@ -4,7 +4,7 @@ from video2img import Video
 
 
 def run_video():
-    video = Video(file0.get(), file1.get(), file2.get(), file3.get(), file4.get())
+    video = Video(files[0].get(), files[1].get(), files[2].get(), files[3].get(), files[4].get())
     video.read_video()
     video.write()
     video.open_dir()
@@ -17,34 +17,27 @@ root.title('課題1')
 frame = ttk.Frame(root, padding=10)
 frame.grid()
 
-load_v = 'v'
-text0 = '処理する動画ファイル'
-num0 = 0
-file0 = create_gui(frame, text0, num0, load_v)
+texts = [
+    '処理する動画ファイル',
+    'RGBカラー画像の出力先フォルダ',
+    'R成分画像の出力先フォルダ',
+    'G成分画像の出力先フォルダ',
+    'B成分画像の出力先フォルダ'
+    ]
 
-load_f = 'f'
-text1 = 'RGBカラー画像の出力先フォルダ'
-num1 = 1
-file1 = create_gui(frame, text1, num1, load_f)
-
-text2 = 'R成分画像の出力先フォルダ'
-num2 = 2
-file2 = create_gui(frame, text2, num2, load_f)
-
-text3 = 'G成分画像の出力先フォルダ'
-num3 = 3
-file3 = create_gui(frame, text3, num3, load_f)
-
-text4 = 'B成分画像の出力先フォルダ'
-num4 = 4
-file4 = create_gui(frame, text4, num4, load_f)
-
+files = []
+for i in range(len(texts)):
+    if i == 0:
+        load = 'v'
+    else:
+        load = 'f'
+    file = create_gui(frame, texts[i], i, load)
+    files.append(file)
 
 run_cmd = run_video
 sub_frame = Subframe(frame)
 sub_frame.create_subfame()
 sub_frame.create_start_button(run_cmd)
 sub_frame.create_exit_button()
-
 
 root.mainloop()
