44039e37bf96070d9f7af1af8eed5a48e600c5b7
candy12t <candy12t@gmail.com>
2020-02-03 14:30:47 +0900

    コメント追加

--- src/task1.py
+++ src/task1.py
@@ -3,6 +3,7 @@ from tkinter import Tk, ttk
 from video2img import Video
 
 
+# 動画処理
 def run_video():
     video = Video(files[0].get(), files[1].get(), files[2].get(), files[3].get(), files[4].get())
     video.read_video()
@@ -11,9 +12,11 @@ def run_video():
     exit()
 
 
+# rootの作成
 root = Tk()
 root.title('課題1')
 
+# frameの作成
 frame = ttk.Frame(root, padding=10)
 frame.grid()
 
@@ -25,15 +28,18 @@ texts = [
     'B成分画像の出力先フォルダ'
     ]
 
+# GUI表示部分の作成
 files = []
 for i in range(len(texts)):
     if i == 0:
         cmd = 'v'
     else:
         cmd = 'f'
+    # ファイルパスを取得し、リストに追加
     file = create_gui(frame, texts[i], i, cmd)
     files.append(file)
 
+# 実行、キャンセルボタン部分のフレーム作成
 run_cmd = run_video
 sub_frame = Subframe(frame)
 sub_frame.create_subfame()
