22aed149ddf1f0416a476c88170f4ba1d5cfe71f
candy12t <candy12t@gmail.com>
2020-02-03 14:09:46 +0900

    コメントの追加

--- src/video2img.py
+++ src/video2img.py
@@ -14,23 +14,27 @@ class Video:
         self.b = b
         self.n = 0
 
+    # 動画の読み込み
     def read_video(self):
-        self.video = cv2.VideoCapture(self.mp4)
-        if not self.video.isOpened():
+        self.video = cv2.VideoCapture(self.mp4) # 動画の読み込み
+        if not self.video.isOpened(): # 動画ファイルとして開けるか
             return
 
+    # 画像の保存
     def save_img(self, dirc, x=None, y=None):
         img = self.img.copy()
-        if x is None and y is None:
+        # x, yで指定した成分の値を0に
+        if (x is None) and (y is None):
             img = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
         else:
             img[:, :, (x, y)] = 0
-        img = Image.fromarray(img)
-        img.save('{}/{}.{}'.format(dirc, self.n, 'png'))
+        img = Image.fromarray(img) # nadarray型からImage型に変換
+        img.save('{}/{}.{}'.format(dirc, self.n, 'png')) #  指定したディレクトリに保存
 
+    # 動画をフレームごとに切り出して処理
     def write(self):
         while True:
-            ret, frame = self.video.read()
+            ret, frame = self.video.read() # (ret, frame) = (boolean型、ndarray型)
             if ret:
                 self.img = np.array(frame)
 
@@ -43,7 +47,9 @@ class Video:
             else:
                 return
 
+    # 指定したディレクトリを開く
     def open_dir(self):
-        input_dir = os.path.dirname(os.path.abspath(self.mp4))
+        input_dir = os.path.dirname(os.path.abspath(self.mp4)) # 動画ファイルのカレントディレクトリの取得
+        # openコマンドの実行
         cmd = 'open {} {} {} {} {}'.format(input_dir, self.rgb, self.r, self.g, self.b)
         subprocess.call(cmd.split())
